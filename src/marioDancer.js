7eb3c6a7d5164a899843c4c9faf7b21908643788
Zachary Lester <zachary.greg.lester@gmail.com>
2015-05-21 10:06:14 -0700

    Fix mario transp

--- src/marioDancer.js
+++ src/marioDancer.js
@@ -3,7 +3,7 @@ var marioFrames = ["src/Resources/mario0.png","src/Resources/mario1.png"];
 var MarioDancer = function (top, left, timeBetweenSteps) {
   ProtoDancer.call(this, top, left, timeBetweenSteps);
 
-  this.$node.prepend('<img src="src/Resources/mario0.png" />');
+  this.$node.append('<img src="src/Resources/mario0.png" />');
   this.$node.addClass("marioDancer");
 
 };
@@ -12,9 +12,12 @@ MarioDancer.prototype = Object.create(ProtoDancer.prototype);
 MarioDancer.prototype.constructor = MarioDancer;
 MarioDancer.prototype.step = function () {
   ProtoDancer.prototype.step.call(this);
-  // this.$node.attr('src', marioFrames[0]);
-  // this.$node.delay(125);
-  this.$node.attr('src', marioFrames[1])
+  this.$node.attr('src', marioFrames[0]);
+  console.log(this.$node.attr('src'));
+  this.$node.delay(125);
+  console.log(this.$node);
+  this.$node.attr('src', marioFrames[1]);
+    console.log(this.$node.attr('src'));
   this.$node.animate({
     'marginBottom': "+=30px"
   }).delay(125).animate({
